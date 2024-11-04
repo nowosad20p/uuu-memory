77ea97f2e06d9e473b14a3afb0993d7b12c6672b
nowosad20p <91263440+nowosad20p@users.noreply.github.com>
2024-11-04 12:03:31 +0100

    Add files via upload

---
+++ Memory/Memory/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Memory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opcjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowaGraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gamePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(126, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(125, 22);
+            this.toolStripMenuItem1.Text = "Nowa gra";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opcjeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opcjeToolStripMenuItem
+            // 
+            this.opcjeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowaGraToolStripMenuItem});
+            this.opcjeToolStripMenuItem.Name = "opcjeToolStripMenuItem";
+            this.opcjeToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.opcjeToolStripMenuItem.Text = "Opcje";
+            // 
+            // nowaGraToolStripMenuItem
+            // 
+            this.nowaGraToolStripMenuItem.Name = "nowaGraToolStripMenuItem";
+            this.nowaGraToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.nowaGraToolStripMenuItem.Text = "Nowa gra";
+            this.nowaGraToolStripMenuItem.Click += new System.EventHandler(this.nowaGraToolStripMenuItem_Click);
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.ColumnCount = 2;
+            this.gamePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.gamePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.gamePanel.Location = new System.Drawing.Point(12, 27);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.RowCount = 2;
+            this.gamePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.03604F));
+            this.gamePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 63.96396F));
+            this.gamePanel.Size = new System.Drawing.Size(776, 411);
+            this.gamePanel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gamePanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opcjeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowaGraToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel gamePanel;
+    }
+}
+
---
+++ Memory/Memory/Form1.cs
@@ -0,0 +1,76 @@
+﻿using Memory.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory
+{
+    public partial class Form1 : Form
+    {
+        int gameSize;
+        List<Image> images = new List<Image>
+        {
+            Resources.mem1,
+            Resources.mem11,
+            Resources.mem2,
+            Resources.mem3,
+            Resources.mem4,
+            Resources.mem5,
+            Resources.mem6,
+            Resources.mem7
+
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip2_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void nowaGraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 ilosc = new Form2();
+          
+            if (ilosc.ShowDialog(this) == DialogResult.OK)
+            {
+                gameSize =  int.Parse(ilosc.textBox1.Text);
+                MessageBox.Show(ilosc.textBox1.Text);
+            }
+            startGame();
+        }
+        private void startGame()
+        {
+            Tuple<int, int> gameDimensions = FindClosestDivisors(gameSize*2);
+            gamePanel.RowCount = gameDimensions.Item1;
+            gamePanel.ColumnCount = gameDimensions.Item2;
+            for (int i = 0; i < gameDimensions.Item1; i++)
+            {
+                for (int j = 0; j < gameDimensions.Item2; j++)
+                {
+                  var img =  new PictureBox { Image = images[new Random().Next(0, images.Count)]};
+                    gamePanel.Controls.Add(img, j,i);
+                }
+            }
+        }
+        static Tuple<int, int> FindClosestDivisors(int n)
+        {
+            int sqrtN = (int)Math.Sqrt(n); 
+            for (int i = sqrtN; i >= 1; i--) {
+                if (n % i == 0) {
+                    int divisor1 = i;
+                    int divisor2 = n / i;
+                    return Tuple.Create(divisor1, divisor2); } 
+            }
+            return Tuple.Create(1, n); 
+        }
+    }
+}
