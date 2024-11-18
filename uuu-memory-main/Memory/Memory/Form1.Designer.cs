538f81699fb3921c2829990370cc0ad85272011b
nowosad20p <91263440+nowosad20p@users.noreply.github.com>
2024-11-18 11:31:44 +0100

    uuuu

---
+++ uuu-memory-main/Memory/Memory/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Memory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opcjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowaGraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gamePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(126, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(125, 22);
+            this.toolStripMenuItem1.Text = "Nowa gra";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opcjeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opcjeToolStripMenuItem
+            // 
+            this.opcjeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowaGraToolStripMenuItem});
+            this.opcjeToolStripMenuItem.Name = "opcjeToolStripMenuItem";
+            this.opcjeToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.opcjeToolStripMenuItem.Text = "Opcje";
+            // 
+            // nowaGraToolStripMenuItem
+            // 
+            this.nowaGraToolStripMenuItem.Name = "nowaGraToolStripMenuItem";
+            this.nowaGraToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.nowaGraToolStripMenuItem.Text = "Nowa gra";
+            this.nowaGraToolStripMenuItem.Click += new System.EventHandler(this.nowaGraToolStripMenuItem_Click);
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.ColumnCount = 2;
+            this.gamePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.5F));
+            this.gamePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.5F));
+            this.gamePanel.Location = new System.Drawing.Point(12, 54);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.gamePanel.RowCount = 2;
+            this.gamePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.03604F));
+            this.gamePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 63.96396F));
+            this.gamePanel.Size = new System.Drawing.Size(600, 600);
+            this.gamePanel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Obecnie ruch gracza ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(476, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wynik: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 658);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gamePanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opcjeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowaGraToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel gamePanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ uuu-memory-main/Memory/Memory/Form1.cs
@@ -0,0 +1,229 @@
+﻿using Memory.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace Memory
+{
+    public partial class Form1 : Form
+    {
+        int gameSize;
+        List<Image> images = new List<Image>
+        {
+            Resources.mem1,
+            Resources.mem2,
+            Resources.mem3,
+            Resources.mem4,
+            Resources.mem5,
+            Resources.mem6,
+            Resources.mem7,
+            Resources.mem8
+
+        };
+        Image background = Resources.pilka_nozna_215cm_bialyczarny_MO9007_33;
+        Tuple<int, int> card1;
+        Tuple<int, int> card2;
+       
+        List<int[]> gameBoard;
+        List<PictureBox[]> gameImages;
+        int curTurn = 0;
+        int[] score = new int[2] { 0, 0 };
+        bool waiting = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip2_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void nowaGraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gamePanel.Controls.Clear();
+            Form2 ilosc = new Form2();
+          
+            if (ilosc.ShowDialog(this) == DialogResult.OK)
+            {
+                gameSize =  int.Parse(ilosc.textBox1.Text);
+                if (gameSize > images.Count)
+                {
+                    gameSize = 7;
+                    MessageBox.Show("Ilość par przekroczyła maksymalną. Ustawiono na " + images.Count);
+                }
+                if (gameSize <= 1)
+                {
+                    gameSize = 5;
+                    MessageBox.Show("Ilość nie może być mniejsza od 2. Ustawiono 5");
+                }
+            }
+            startGame();
+        }
+         public static void ShuffleGameBoard(List<int[]> gameBoard, Tuple<int, int> gameDimensions)
+        {
+            List<int> flattened = new List<int>(); 
+            for (int i = 0; i < gameDimensions.Item1; i++) { for (int j = 0; j < gameDimensions.Item2; j++)
+                {
+                    flattened.Add(gameBoard[i][j]);
+                } }
+            Random rand = new Random();
+            for (int i = flattened.Count - 1; i > 0; i--)
+            { int j = rand.Next(0, i + 1); 
+                int temp = flattened[i]; 
+                flattened[i] = flattened[j]; 
+                flattened[j] = temp; }
+        
+            int index = 0;
+            for (int i = 0; i < gameDimensions.Item1; i++)
+            {
+                for (int j = 0; j < gameDimensions.Item2; j++) 
+                { gameBoard[i][j] = flattened[index++]; 
+                }
+            }
+
+        }
+            private void startGame()
+            {
+
+           
+
+                Tuple<int, int> gameDimensions = FindClosestDivisors(gameSize*2);
+            gameBoard = new List<int[]>();
+            gameImages = new List<PictureBox[]>();
+            int n = 0;
+            int k = 0;
+            for(int i=0; i<gameDimensions.Item1; i++)
+            {
+                gameBoard.Add(new int[gameDimensions.Item2]);
+                gameImages.Add(new PictureBox[gameDimensions.Item2]);
+                for(int j = 0; j < gameDimensions.Item2; j++)
+                {
+                    gameBoard[i][j] = n;
+                    n += (k% 2);
+                    k++;
+                }
+            }
+      
+            ShuffleGameBoard(gameBoard,gameDimensions);
+         
+            gamePanel.RowCount = gameDimensions.Item1;
+            gamePanel.ColumnCount = gameDimensions.Item2;
+            for (int i = 0; i < gamePanel.ColumnCount; i++) 
+            {
+                gamePanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100 / gamePanel.RowCount)); 
+            }
+            for (int j = 0; j < gamePanel.RowCount; j++) 
+            {
+                gamePanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100/gamePanel.RowCount)); 
+            }
+         
+            for (int i = 0; i < gameDimensions.Item1; i++)
+            {
+                for (int j = 0; j < gameDimensions.Item2; j++)
+                {
+                    int row = i;
+                    int col = j;
+                    var img =  new PictureBox { Image = background , Parent = gamePanel, SizeMode = PictureBoxSizeMode.StretchImage,Width = gamePanel.Width/gamePanel.ColumnCount, Height = gamePanel.Width/gamePanel.ColumnCount, BorderStyle = BorderStyle.FixedSingle};
+                    img.Click += (sender, e) =>
+                    {
+                        flip(row,col);
+                    };
+                    gameImages[i][j] = img;
+                    gamePanel.Controls.Add(img, j,i);
+              
+
+                }
+            }
+        }
+        void flip(int x, int y)
+        {
+            if(waiting) return;
+            gameImages[x][y].Image = images[gameBoard[x][y]];
+       
+            if (card1 != null)
+            {
+               
+                card2 = Tuple.Create(x, y);
+                
+                if (gameBoard[card1.Item1][card1.Item2] == gameBoard[card2.Item1][card2.Item2])
+                {
+                 
+                    score[curTurn]++;
+                    if (score[0] + score[1] == gameSize)
+                    {
+                        if (score[0] == score[1])
+                        {
+                            MessageBox.Show("Remis","Wynik",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+
+
+                            string winner = score[0] > score[1] ? "1" : "2";
+                            MessageBox.Show("Wygrał gracz numer " + winner, "Wynik", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                    label2.Text = "Wynik: " + score[0].ToString() + ":" + score[1].ToString();
+                    waiting = true;
+                    Task.Delay(500).ContinueWith(_ => {
+                        gameImages[x][y].Invoke((Action)(() => gameImages[x][y].Visible = false));
+                        gameImages[card1.Item1][card1.Item2].Invoke((Action)(() => gameImages[card1.Item1][card1.Item2].Visible = false));
+                        card1 = null;
+                        card2 = null;
+                        waiting = false;
+                    });
+                }
+                else
+                {
+
+
+                    if (curTurn == 0)
+                    {
+                        curTurn = 1;
+                    }
+                    else
+                    {
+                        curTurn = 0;
+                    }
+                    label1.Text = "Obecnie tura gracza " + (curTurn + 1).ToString();
+                    waiting = true;
+                    Task.Delay(1000).ContinueWith(_ => {
+                        gameImages[x][y].Invoke((Action)(() => gameImages[x][y].Image = background));
+                        gameImages[card1.Item1][card1.Item2].Invoke((Action)(() => gameImages[card1.Item1][card1.Item2].Image = background));
+                        card1 = null;
+                        card2 = null;
+                        waiting = false;
+                    });
+                }
+
+                
+                
+            }
+            else
+            {
+                card1 = Tuple.Create(x, y);
+                
+            }
+            
+        }
+        static Tuple<int, int> FindClosestDivisors(int n)
+        {
+            int sqrtN = (int)Math.Sqrt(n); 
+            for (int i = sqrtN; i >= 1; i--) {
+                if (n % i == 0) {
+                    int divisor1 = i;
+                    int divisor2 = n / i;
+                    return Tuple.Create(divisor1, divisor2); } 
+            }
+            return Tuple.Create(1, n); 
+        }
+    }
+}
